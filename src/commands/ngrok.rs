f3c26fa1028535899e6f16af9c3f6df2ba524fee
Suryansh <suryansh5060@gmail.com>
2022-12-12 13:53:12 +0530

    Add set-ngrok command

--- src/commands/ngrok.rs
+++ src/commands/ngrok.rs
@@ -1,3 +1,29 @@
-pub fn set_ngrok() {
-    println!("Setting the ngrok path!");
+use std::fs;
+use std::io::prelude::*;
+use home;
+
+pub fn set_ngrok(args: &Vec<String>) {
+    // Path value is at index 2 
+    let filepath = &args[2];
+
+    // Get home dir 
+    match home::home_dir() {
+        Some(path) => {
+            let write_path: &String = &format!("{}/.chicken-biryani/", path.display());
+
+            // Making the directory
+            fs::DirBuilder::new()
+                .recursive(true)
+                .create(write_path).unwrap();
+
+            // Create the config.toml
+            let file = fs::File::create(format!("{}config.toml", write_path));
+
+            // Write to the file
+            _ = file.expect("Couldn't open file").write_all(format!("ngrok = \"{}\"", filepath).as_bytes());
+            
+            println!("ngrok path has been set!");
+        },
+        None => println!("Couldn't get your home directory!"),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // Perform the following operations
-    // 1. `ngrok path`: Set ngrok location in config file at ~/.chicken-biryani/config.toml
+    // 1. `set-ngrok path`: Set ngrok location in config file at ~/.chicken-biryani/config.toml
     // 2. `host filepath port`: Start a server on optionally specified port with the file at filepath hosted. If ngrok path is set,
     //    http tunnel the opened port using ngrok. Otherwise, if the machine has a static ip, run
     //    on that.
@@ -21,7 +21,7 @@ fn main() {
 
     // Run the command module for the specific command
     match command.as_str() {
-        "set-ngrok" => commands::ngrok::set_ngrok(),
+        "set-ngrok" => commands::ngrok::set_ngrok(&args),
         "host" => commands::host::host(),
         _ => println!("Invalid command!"),
     };
