88c2477710423f952b2d918a143c67dc1f904a6c
Suryansh <suryansh5060@gmail.com>
2022-12-12 15:59:32 +0530

    Use ChatGPT to optimize code

--- src/commands/ngrok.rs
+++ src/commands/ngrok.rs
@@ -3,25 +3,23 @@ use std::io::prelude::*;
 use home;
 
 pub fn set_ngrok(args: &Vec<String>) {
-    // Path value is at index 2 
+    // Path value is at index 2
     let filepath = &args[2];
 
-    // Get home dir 
+    // Get home dir
     match home::home_dir() {
         Some(path) => {
-            let write_path: &String = &format!("{}/.chicken-biryani/", path.display());
+            let write_path = format!("{}/.chicken-biryani/", path.display());
 
             // Making the directory
-            fs::DirBuilder::new()
-                .recursive(true)
-                .create(write_path).unwrap();
+            fs::create_dir_all(&write_path).unwrap();
 
             // Create the config.toml
-            let file = fs::File::create(format!("{}config.toml", write_path));
+            let mut file = fs::File::create(format!("{}config.toml", write_path)).unwrap();
 
             // Write to the file
-            _ = file.expect("Couldn't open file").write_all(format!("ngrok = \"{}\"", filepath).as_bytes());
-            
+            file.write_all(format!("ngrok = \"{}\"", filepath).as_bytes()).unwrap();
+
             println!("ngrok path has been set!");
         },
         None => println!("Couldn't get your home directory!"),
